7c2b403bad922ef9e79e7877c148a1566ea6fe07
test <1781767218@qq.com>
2020-11-30 15:53:15 +0800

    就上传一下

--- src/main/java/com/mofil/controller/TestController.java
+++ src/main/java/com/mofil/controller/TestController.java
@@ -3,7 +3,6 @@ package com.mofil.controller;
 public class TestController {
     public void say(){
         System.out.println("真就测试呗");
-        System.out.println("真就测试呗1");
-        System.out.println("真就测试呗2");
+
     }
 }

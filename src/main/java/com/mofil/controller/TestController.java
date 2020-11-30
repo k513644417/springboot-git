5abc1d7cc96f999da1c855d4d536696e09e18044
test <1781767218@qq.com>
2020-11-30 15:44:31 +0800

    就更新一下

--- src/main/java/com/mofil/controller/TestController.java
+++ src/main/java/com/mofil/controller/TestController.java
@@ -3,5 +3,7 @@ package com.mofil.controller;
 public class TestController {
     public void say(){
         System.out.println("真就测试呗");
+        System.out.println("真就测试呗1");
+        System.out.println("真就测试呗2");
     }
 }

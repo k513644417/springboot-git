921f2668f9720b5d09ec6cf267a74a99b73ae9b5
test <1781767218@qq.com>
2020-11-30 16:32:32 +0800

    添加返回string

--- src/main/java/com/mofil/controller/HelloController.java
+++ src/main/java/com/mofil/controller/HelloController.java
@@ -4,5 +4,7 @@ import org.springframework.stereotype.Component;
 
 
 public class HelloController {
-
+    public String hello(){
+        return "你好，欢迎光临，这个项目是我的第一个git项目，只用做学习git的一些基本操作";
+    }
 }
